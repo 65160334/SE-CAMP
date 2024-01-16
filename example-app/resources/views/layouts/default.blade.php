ee9390b33f8f36666ab192189ce0f96d33beca17
65160334 <boatgamer89@gmail.com>
2024-01-16 23:18:39 +0700

    jui

--- example-app/resources/views/layouts/default.blade.php
+++ example-app/resources/views/layouts/default.blade.php
@@ -112,15 +112,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         <a href="#" class="dropdown-item">
                             <!-- Message Start -->
                             <div class="media">
-                                <img src="{{ url('assets/dist/img/user3-128x128.jpg') }}" alt="User Avatar"
+                                <img src="{{ url('assets/dist/img/user6-128x128.jpg') }}" alt="User Avatar"
                                     class="img-size-50 img-circle mr-3">
                                 <div class="media-body">
                                     <h3 class="dropdown-item-title">
-                                        Nora Silvester
+                                        Thanakitty UWU
                                         <span class="float-right text-sm text-warning"><i
                                                 class="fas fa-star"></i></span>
                                     </h3>
-                                    <p class="text-sm">The subject goes here</p>
+                                    <p class="text-sm">กินลาบบ่อ้าย</p>
                                     <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                                 </div>
                             </div>
